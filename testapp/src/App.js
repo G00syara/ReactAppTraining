4c57b0775387403bb65a80853fd6cec0e282e77e
G00syara <sasha.ugarov.02@mail.ru>
2023-06-14 16:02:42 +0300

    7 commit first verison how to do different pagws

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -15,20 +15,29 @@ import './css/app.scss';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './components/utils/pages';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  let pagesArray = getPagesArray(totalPages);
+
+  console.log(pagesArray);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -37,6 +46,10 @@ function App() {
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
   };
 
   return (
@@ -59,6 +72,13 @@ function App() {
         ) : (
           <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов" />
         )}
+        <div className="page__wrapper">
+          {pagesArray.map((p) => (
+            <span onClick={() => changePage(p)} key={p} className={page === p ? 'page page__current' : 'page'}>
+              {p}
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );

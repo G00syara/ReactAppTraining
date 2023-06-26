aa62b1d5a0d863a7254cdbaff403f2b57b655b86
Александр Угаров <augarov@web-bee.ru>
2023-06-26 21:08:21 +0300

    check new git name and email

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,9 +1,6 @@
 import React, { useState, useRef, useMemo, useEffect } from 'react';
 import './css/app.scss';
 import { BrowserRouter } from 'react-router-dom';
-import About from './pages/About';
-import Posts from './pages/Posts';
-import Error from './pages/Error';
 import Navbar from './components/UI/navbar/Navbar';
 import AppRouter from './components/AppRouter';
 import { AuthContext } from './context';
